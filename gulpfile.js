35c1ac8ec2c17e0104f34ab7b7028b9b0f0bdb01
Luzlum <uebung783@gmail.com>
2015-03-15 01:40:06 +0900

    add ejs and more

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,22 @@ var less = require('gulp-less');
 var plumber = require('gulp-plumber');
 var minifyCSS = require('gulp-minify-css');
 var notify = require('gulp-notify');
+var autoprefixer = require('gulp-autoprefixer');
+var ejs = require('gulp-ejs');
 
 gulp.task('minify', function(){
     gulp.src('./htdocs/css/*.css')
     .pipe(minifyCSS())
     .pipe(gulp.dest('./htdocs/prod/css'));
+});
+
+gulp.task('ejs', function(){
+    gulp.src(['./htdocs/ejs/**/*.ejs', '!' + './htdocs/ejs/**/_*.ejs'])
+    .pipe(plumber({
+        errorHandler: notify.onError('Error: <%= error.message %>')//デスクトップに通知
+    }))
+    .pipe(ejs())
+    .pipe(gulp.dest('./htdocs/prod'));
 });
 
 gulp.task('less', function(){
@@ -16,11 +27,13 @@ gulp.task('less', function(){
         errorHandler: notify.onError('Error: <%= error.message %>')//デスクトップに通知
     })) //lessのコンパイルでこけても終了しない
     .pipe(less())
+    .pipe(autoprefixer())
     .pipe(gulp.dest('./htdocs/css'));
 });
 
 gulp.task('watch', function(){
     gulp.watch('./htdocs/less/*.less', ['less']);
+    gulp.watch(['./htdocs/ejs/**/*.ejs', '!' + './htdocs/ejs/**/_*.ejs'], ['ejs']);
 });
 
-gulp.task('default', ['less']);
+gulp.task('default', ['watch']);
