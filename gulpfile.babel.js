35d6e99544c3911481967d35e1a9e96ce1e5b898
qurage <qualle.rhythmus+d@gmail.com>
2016-05-10 20:30:25 +0900

    refactor paths
    * rename 'paths' module to 'logger'
    * export as ES Modules not CommonJS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,42 +3,30 @@
 import del from 'del';
 
 import gulp from 'gulp';
-import {log, colors} from 'gulp-util';
 import pug from 'gulp-pug';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 import purify from 'gulp-purifycss'
 
 import {dest, templates, style} from './tasks/config';
-import {logPaths} from './tasks/paths'
+import Logger from './tasks/logger'
 
+const {logDeleted} = Logger;
 gulp.task('default', [
   'pug',
   'sass'
 ]);
 
-gulp.task('clean:all', (done) => {
-  del([dest]).then(paths => {
-    log('Deleted files and folders:');
-    logPaths(paths);
-    return done();
-  });
+gulp.task('clean:all', () => {
+  return del([dest]).then(logDeleted);
 });
 
-gulp.task('clean:html', (done) => {
-  del([templates.dest]).then(paths => {
-    log('Deleted files and folders:');
-    logPaths(paths);
-    return done();
-  });
+gulp.task('clean:html', () => {
+  return del([templates.dest]).then(logDeleted);
 });
 
-gulp.task('clean:style', (done) => {
-  del([style.dest]).then(paths => {
-    log('Deleted files and folders:');
-    logPaths(paths);
-    return done();
-  });
+gulp.task('clean:style', () => {
+  return del([style.dest]).then(logDeleted);
 });
 
 gulp.task('pug', ['clean:html'], () => {
--- tasks/logger.js
+++ tasks/logger.js
@@ -2,19 +2,24 @@
 
 import path from 'path';
 
-import {colors} from 'gulp-util';
+import {log, colors} from 'gulp-util';
 const {magenta} = colors;
 
 import {root} from './config';
 
-module.exports =  {
-  logPaths: (paths) => {
+export default class Logger {
+  static logPaths(paths) {
     if (paths.length === 0) {
       return console.log('Nothing.');
     }
     paths.forEach(p => {
       p = path.relative(root, p);
-      console.log(colors.magenta(p));
+      console.log(magenta(p));
     });
+  }
+
+  static logDeleted(paths) {
+    log('Deleted files and folders:');
+    Logger.logPaths(paths);
   }
 };
