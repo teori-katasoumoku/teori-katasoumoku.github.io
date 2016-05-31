c18897cf8214a5d1a40f155afd68d7aa8ba5b96f
qrg <qualle.rhythmus+d@gmail.com>
2016-05-31 10:19:51 +0900

    Refactor - mode.js
    * cut out logging state as new method `logState(stateName)`

--- gulpfile.babel.js/lib/mode.js
+++ gulpfile.babel.js/lib/mode.js
@@ -1,19 +1,31 @@
 'use strict';
 
+import {colors} from 'gulp-util';
+const {yellow} = colors;
+
+const mandatory = () => {
+  throw new Error('Missing parameter: state name');
+};
+
 class Mode {
   constructor() {
     this._watching = false;
-    console.log('\nwatching mode: false\n');
+    this.logState('watching');
   }
 
   get watching() {
     return this._watching;
   }
 
-  set watching(value) {
-    this._watching = value;
-    console.log('\nwatching mode: true\n');
-    return this._watching
+  set watching(bool) {
+    if (typeof bool !== 'boolean') throw new Error('set true or false as parameter');
+    this._watching = bool;
+    this.logState('watching');
+    return this._watching;
+  }
+
+  logState(stateName = mandatory()) {
+    console.log(`\n${stateName} mode: ${yellow(this[stateName])}\n`);
   }
 }
 
